24fe7017bb939e495d3cbce4fb78682166ed832c
wanto-production <ikwansatria3974@gmail.com>
2026-05-23 13:38:58 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: { preset: 'vercel' },
   runtimeConfig: {
     public: {
       appBaseUrl: process.env.PUBLIC_APP_BASE_URL,
